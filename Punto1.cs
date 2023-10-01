881a8abd807d6c1a44d55fdd8aa59dc56aaa869b
Villa0416 <104180044+Villa0416@users.noreply.github.com>
2023-09-30 19:33:40 -0500

    Se agrega el entregable

---
+++ Punto1.cs
@@ -0,0 +1,97 @@
+﻿
+interface IHabilidad
+{
+    int Ejecutar(int valor);
+}
+
+abstract class Pokemon
+{
+    private string nombre;
+    private string tipo;
+    private List<int> ataques;
+    private int defensa;
+
+    public Pokemon(string nombre, string tipo, List<int> ataques, int defensa)
+    {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.ataques = ataques;
+        this.defensa = defensa;
+    }
+
+    public int Atacar()
+    {
+        Random rand = new Random();
+        int ataqueSeleccionado = ataques[rand.Next(ataques.Count)];
+        int puntaje = rand.Next(0, 41); 
+        return ataqueSeleccionado * puntaje;
+    }
+
+    public int Defender()
+    {
+        return defensa * 5 / 10; 
+    }
+}
+
+
+class PokemonEspecifico : Pokemon
+{
+    public PokemonEspecifico(string nombre, string tipo, List<int> ataques, int defensa)
+        : base(nombre, tipo, ataques, defensa)
+    {
+    }
+}
+
+
+class BatallaPokemon
+{
+    private Pokemon pokemon1;
+    private Pokemon pokemon2;
+
+    public BatallaPokemon(Pokemon pokemon1, Pokemon pokemon2)
+    {
+        this.pokemon1 = pokemon1;
+        this.pokemon2 = pokemon2;
+    }
+
+    public string SimularBatalla()
+    {
+        int puntajePokemon1 = 0;
+        int puntajePokemon2 = 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            int ataque1 = pokemon1.Atacar();
+            int defensa1 = pokemon2.Defender();
+            int ataque2 = pokemon2.Atacar();
+            int defensa2 = pokemon1.Defender();
+
+            puntajePokemon1 += ataque1 * defensa1;
+            puntajePokemon2 += ataque2 * defensa2;
+        }
+
+        if (puntajePokemon1 > puntajePokemon2)
+            return $"{pokemon1} gana la batalla.";
+        else if (puntajePokemon2 > puntajePokemon1)
+            return $"{pokemon2} gana la batalla.";
+        else
+            return "Empate.";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<int> ataques1 = new List<int> { 10, 20, 30 };
+        List<int> ataques2 = new List<int> { 15, 25, 35 };
+
+        Pokemon pokemon1 = new PokemonEspecifico("Pokemon1", "Tipo1", ataques1, 30);
+        Pokemon pokemon2 = new PokemonEspecifico("Pokemon2", "Tipo2", ataques2, 25);
+
+        BatallaPokemon batalla = new BatallaPokemon(pokemon1, pokemon2);
+        string resultado = batalla.SimularBatalla();
+
+        Console.WriteLine(resultado);
+    }
+}
---
+++ Punto2.cs
@@ -0,0 +1,105 @@
+﻿
+
+interface IJugador
+{
+    string Nombre { get; }
+    string Posicion { get; }
+    int Rendimiento { get; }
+}
+
+
+class Jugador : IJugador
+{
+    private string nombre;
+    private string posicion;
+    private int rendimiento;
+
+    public Jugador(string nombre, string posicion, int rendimiento)
+    {
+        this.nombre = nombre;
+        this.posicion = posicion;
+        this.rendimiento = rendimiento;
+    }
+
+    public string Nombre { get { return nombre; } }
+    public string Posicion { get { return posicion; } }
+    public int Rendimiento { get { return rendimiento; } }
+}
+
+
+class Equipo
+{
+    private List<IJugador> jugadores = new List<IJugador>();
+
+    public void AgregarJugador(IJugador jugador)
+    {
+        if (jugadores.Count < 3)
+        {
+            jugadores.Add(jugador);
+        }
+        else
+        {
+            Console.WriteLine("El equipo ya tiene 3 jugadores. No se puede agregar más.");
+        }
+    }
+
+    public int CalcularPuntaje()
+    {
+        return jugadores.Sum(jugador => jugador.Rendimiento);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+        List<IJugador> jugadoresRegistrados = new List<IJugador>
+        {
+            new Jugador("Jugador1", "Base", 7),
+            new Jugador("Jugador2", "Escolta", 6),
+            new Jugador("Jugador3", "Alero", 8),
+            new Jugador("Jugador4", "Pívot", 9),
+            new Jugador("Jugador5", "Ala-Pívot", 7),
+            new Jugador("Jugador6", "Base", 6)
+        };
+
+        
+        Equipo equipo1 = new Equipo();
+        Equipo equipo2 = new Equipo();
+
+        
+        Random random = new Random();
+
+        for (int i = 0; i < 3; i++)
+        {
+            int index1 = random.Next(jugadoresRegistrados.Count);
+            IJugador jugador1 = jugadoresRegistrados[index1];
+            equipo1.AgregarJugador(jugador1);
+            jugadoresRegistrados.RemoveAt(index1);
+
+            int index2 = random.Next(jugadoresRegistrados.Count);
+            IJugador jugador2 = jugadoresRegistrados[index2];
+            equipo2.AgregarJugador(jugador2);
+            jugadoresRegistrados.RemoveAt(index2);
+        }
+
+        
+        int puntajeEquipo1 = equipo1.CalcularPuntaje();
+        int puntajeEquipo2 = equipo2.CalcularPuntaje();
+
+        
+        if (puntajeEquipo1 > puntajeEquipo2)
+        {
+            Console.WriteLine("Equipo 1 gana el partido.");
+        }
+        else if (puntajeEquipo2 > puntajeEquipo1)
+        {
+            Console.WriteLine("Equipo 2 gana el partido.");
+        }
+        else
+        {
+            Console.WriteLine("El partido termina en empate.");
+        }
+    }
+}
